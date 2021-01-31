c2d8b554bc0afe7018bbe1d8bc1f7a39ff477a77
Marcelo Almeida <contato@marceloalmeida.dev>
2021-01-31 11:39:28 -0300

    created Dockerfile to production

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders my name', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const nameElement = screen.getByText(/Marcelo Almeida/i);
+  expect(nameElement).toBeInTheDocument();
+});
+test('renders my name', () => {
+  render(<App />);
+  const nameElement = screen.getByText(/Marcelo Almeida/i);
+  expect(nameElement).toBeInTheDocument();
 });
